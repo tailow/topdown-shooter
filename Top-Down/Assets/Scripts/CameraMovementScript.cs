c78887ba7a432fbb04f75e5f27c64e47a440798f
tailow-pc <revitate@hotmail.com>
2017-11-08 21:17:34 +0200

    Add project file

---
+++ Top-Down/Assets/Scripts/CameraMovementScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour {
+
+    #region Variables
+
+    public float smoothTime;
+
+    Vector3 velocity = Vector3.zero;
+
+    Transform playerTransform;
+    Transform cameraNewPosition;
+
+	#endregion
+
+	void Start () 
+	{
+        // PLAYER TRANSFORM
+        playerTransform = GameObject.Find("PlayerModel").GetComponent<Transform>();
+
+        // CAMERA NEW POSITION
+        cameraNewPosition = GameObject.Find("CameraNewPosition").GetComponent<Transform>();
+
+    }
+	
+	void Update () 
+	{
+        // LOOK AT PLAYER
+        transform.LookAt(playerTransform);
+
+        // MOVE CAMERA
+        transform.position = Vector3.SmoothDamp(transform.position, cameraNewPosition.position, ref velocity, smoothTime);
+    }
+}
